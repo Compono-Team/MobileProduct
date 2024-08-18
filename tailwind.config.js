a4cb8f56f28db6f7a4e474db92d78c8ba381ddd9
seohyeon1578 <seohyeon1578@naver.com>
2024-08-18 19:26:19 +0900

    👷 Chore: add logo image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {colors: {black: '#1c1d1f'}},
   },
   plugins: [],
 };
