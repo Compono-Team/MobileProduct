2c1cd2698ed681571ec02fc28489c0583fb8430c
seohyeon1578 <seohyeon1578@naver.com>
2024-08-22 22:51:17 +0900

    ✨ Feat: Implement KaKao OAuth Login Button

---
+++ src/components/Auth/KaKaoOAuthLoginButton.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect} from 'react';
+import {View, Image, TouchableOpacity} from 'react-native';
+import {login} from '@react-native-kakao/user';
+import {initializeKakaoSDK} from '@react-native-kakao/core';
+import {router} from 'expo-router';
+
+import {useSession} from '@/context/sessionContext';
+
+const KaKaoOAuthLoginButton = () => {
+  const {signIn} = useSession();
+
+  const onKaKaoLoginPress = () => {
+    try {
+      const userInfo = login();
+      console.log(userInfo);
+      signIn();
+      router.replace('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    initializeKakaoSDK('f33b4b02431fe06cb3f8df2cca7be84a');
+  }, []);
+
+  return (
+    <TouchableOpacity
+      className="flex justify-center items-center w-[48px] h-[48px] bg-kakao rounded-full"
+      onPress={onKaKaoLoginPress}>
+      <Image source={require('@/assets/images/auth/kakao-logo.png')} />
+    </TouchableOpacity>
+  );
+};
+
+export default KaKaoOAuthLoginButton;
