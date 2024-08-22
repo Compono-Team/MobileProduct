17fc17972cb0f2a153bbb46c47c0af0bb2617927
seohyeon1578 <seohyeon1578@naver.com>
2024-08-22 22:53:22 +0900

    🐛 Fix: Sign in page with Auth Provider

--- src/app/sign-in.tsx
+++ src/app/sign-in.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Image} from 'react-native';
 
-import Auth from '@/components/Auth';
+import AuthProviders from '@/components/Auth/AuthProviders';
 
 export default function SignIn() {
   return (
     <View className="flex-1 justify-center items-center bg-black pt-20">
-      <Image
-        source={require('@/assets/images/logo.png')}
-        className="h-20"
-      />
-      <Auth />
+      <Image source={require('@/assets/images/logo.png')} className="h-20" />
+      <AuthProviders />
     </View>
   );
 }
