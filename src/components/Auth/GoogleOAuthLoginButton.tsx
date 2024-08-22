33437d70a32591230d3e414bd1f194529832de60
seohyeon1578 <seohyeon1578@naver.com>
2024-08-22 22:50:41 +0900

    ✨ Feat: Implement Google OAuth Login

---
+++ src/components/Auth/GoogleOAuthLoginButton.tsx
@@ -0,0 +1,57 @@
+import React, {useEffect} from 'react';
+import {Image, TouchableOpacity} from 'react-native';
+import {
+  statusCodes,
+  isErrorWithCode,
+  GoogleSignin,
+} from '@react-native-google-signin/google-signin';
+import {router} from 'expo-router';
+
+import {configureGoogleLogin} from '@/utils/googleLogin';
+import {useSession} from '@/context/sessionContext';
+
+const GoogleOAuthLoginButton = () => {
+  const {signIn} = useSession();
+
+  const onGoogleLoginPress = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+      signIn();
+      router.replace('/');
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.SIGN_IN_CANCELLED:
+            // user cancelled the login flow
+            break;
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
+
+  useEffect(() => {
+    configureGoogleLogin();
+  }, []);
+
+  return (
+    <TouchableOpacity
+      className="flex justify-center items-center w-[48px] h-[48px] bg-white rounded-full"
+      onPress={onGoogleLoginPress}>
+      <Image source={require('@/assets/images/auth/google-logo.png')} />
+    </TouchableOpacity>
+  );
+};
+
+export default GoogleOAuthLoginButton;
